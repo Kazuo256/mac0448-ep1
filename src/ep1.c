454a46ceaad9c1b4c16abbdf5f33e477ccc9f043
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-08-21 15:16:18 -0300

    Refactoring to use packet. Weird browser behaviour.

--- src/ep1.c
+++ src/ep1.c
@@ -6,49 +6,53 @@
 #include <string.h>
 #include <unistd.h>
 
-#define MAXLINE 4096
+#define MAXPACKET 4096
 
-/*
-"HTTP/1.1 404 Not Found"
-"Date: Sun, 31 Jul 2011 18:45:56 GMT"
-"Server: Apache/2.2.9"
-"Vary: Accept-Encoding"
-"Content-Encoding: gzip"
-"Content-Length: 186"
-"Keep-Alive: timeout=15, max=100"
-"Connection: Keep-Alive"
-"Content-Type: text/html; charset=iso-8859-1"
-""
-"M10w~.@$A$%)  REow{lN\\BXt1Be=%Jrl/i'PiAp]ixnG<NdLIsJvrl{A`Ug%W~r"
-*/
+typedef struct {
+  char    data[MAXPACKET];
+  size_t  size;
+} EP1_packet;
 
-void send_notfound (int connfd, char sendline[]) {
+static int receive (int connfd, EP1_packet* recvpack) {
+  ssize_t n;
+  n = read(connfd, recvpack->data, MAXPACKET);
+  if (n < 0) return 0;
+  recvpack->data[n] = '\0';
+  recvpack->size = strlen(recvpack->data);/*(size_t)n;*/
+  printf("[Cliente conectado no processo filho %d enviou:]\n",getpid());
+  if ((fputs(recvpack->data,stdout)) == EOF) {
+     perror("fputs :( \n");
+     exit(6);
+  }
+  return 1;
+}
+
+static int send (int connfd, EP1_packet* sendpack) {
+  puts(sendpack->data);
+  return write(connfd, sendpack->data, sendpack->size) >= 0;
+}
+
+static size_t send_notfound (char sendline[]) {
   FILE    *notfound = fopen("packets/notfound", "r");
   size_t  n;
-  n = fread(sendline, 1, MAXLINE, notfound);
+  n = fread(sendline, 1, MAXPACKET, notfound);
   sendline[n] = '\0';
-  /*write(connfd, recvline, strlen(recvline));*/
-  puts(sendline);
-  if (write(connfd, sendline, n) == -1)
-    perror("send packet failed\n");
+  return n;
 }
 
 void EP1_handle_connection (int connfd) {
-  /* Armazena linhas recebidas do cliente */
-  char    recvline[MAXLINE+1];
+  /* Armazena pacotes recebido do cliente */
+  EP1_packet  recvpack;
   /* Armazena linhas a serem enviadas para o cliente */
-  char    sendline[MAXLINE+1];
+  char    sendline[MAXPACKET+1];
   /* Armazena o tamanho da string lida do cliente */
-  ssize_t n;
-  while ((n=read(connfd, recvline, MAXLINE)) > 0) {
-    recvline[n]=0;
-    printf("[Cliente conectado no processo filho %d enviou:]\n",getpid());
-    if ((fputs(recvline,stdout)) == EOF) {
-       perror("fputs :( \n");
-       exit(6);
-    }
+  size_t  m;
+  while (receive(connfd, &recvpack)) {
     /*write(connfd, recvline, strlen(recvline));*/
-    send_notfound(connfd, sendline);
+    m = send_notfound(sendline);
+    puts(sendline);
+    if (write(connfd, sendline, m) == -1)
+      perror("send packet failed\n");
   }
 }
 
