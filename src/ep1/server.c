14bac500e6df514b948d36b91186842e849e3db6
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-08-26 11:25:55 -0300

    O serv acha a página, mas eu acho que ele n tá mandando.

--- src/ep1/server.c
+++ src/ep1/server.c
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 
-void tryToGetPage(const EP1_NET_packet* req, FILE *returnPage) {
+FILE* tryToGetPage(const EP1_NET_packet* req) {
 	char getReq[256], page[256];
 	int i, firstLineSize = 0;
+	FILE* returnPage = NULL;
+
 	for (i = 0; req->data[i] != '\n'; i++) firstLineSize++;
 	if (firstLineSize >= 256) {
 		returnPage = fopen("packets/notfound", "r");
@@ -16,19 +18,24 @@ void tryToGetPage(const EP1_NET_packet* req, FILE *returnPage) {
 		if (strncmp("GET ", getReq, 4) == 0 ) {
 			for (i = 4; i < 252; i++ ) {
 				if (strncmp("HTTP", getReq+i, 4) == 0) {
-					strncpy(page, getReq+4, i-4);
-					page[i-5] = '\0';
+					strcpy(page, ".www");
+					strncat(page, getReq+4, i-4);
+					page[i-1] = '\0';
+					puts(page);
+					returnPage = fopen(page, "r");
+					if (returnPage == NULL) {
+						returnPage = fopen("packets/notfound", "r");
+					}
 				}
 			}
 		}
 	}
+	return returnPage;
 }
 
 void EP1_SERVER_respond (const EP1_NET_packet* req, EP1_NET_packet* resp) {
-	FILE *returnPage = NULL; 
+	FILE *returnPage = tryToGetPage(req);
 
-	tryToGetPage(req, returnPage);
-  
   /* Lê o arquivo e guarda os dados no pacote */
   resp->size = fread(resp->data, 1, EP1_PACKETSIZE, returnPage);
   resp->data[resp->size] = '\0';
