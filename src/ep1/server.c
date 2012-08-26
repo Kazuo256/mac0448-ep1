dda5642c801ae766da887cf14c455a815f95f89d
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-08-26 10:58:28 -0300

    O servidor consegue pegar o caminho correto da página.

--- src/ep1/server.c
+++ src/ep1/server.c
@@ -4,24 +4,28 @@
 #include <stdio.h>
 #include <string.h>
 
-void tryToGetPage(const EP1_NET_packet* req, FILE* returnPage) {
-	char page[256];
+void tryToGetPage(const EP1_NET_packet* req, FILE *returnPage) {
+	char getReq[256], page[256];
 	int i, firstLineSize = 0;
 	for (i = 0; req->data[i] != '\n'; i++) firstLineSize++;
 	if (firstLineSize >= 256) {
 		returnPage = fopen("packets/notfound", "r");
 	} else {
-		strncpy(page, req->data, firstLineSize);
-		page[firstLineSize] = '\0';
-		if (strcmp("GET", strncpy(NULL, req->data, 3)) == 0 ) {
-		puts(page);
-
+		strncpy(getReq, req->data, firstLineSize);
+		getReq[firstLineSize] = '\0';
+		if (strncmp("GET ", getReq, 4) == 0 ) {
+			for (i = 4; i < 252; i++ ) {
+				if (strncmp("HTTP", getReq+i, 4) == 0) {
+					strncpy(page, getReq+4, i-4);
+					page[i-5] = '\0';
+				}
+			}
 		}
 	}
 }
 
 void EP1_SERVER_respond (const EP1_NET_packet* req, EP1_NET_packet* resp) {
-	FILE *returnPage; 
+	FILE *returnPage = NULL; 
 
 	tryToGetPage(req, returnPage);
   
